60328fa709e893fb1d18762dbfc3bd968382d86a
Sergei Averkiev <averoo@gmail.com>
2024-10-04 17:27:39 +0300

    Add log.

--- make_page.py
+++ make_page.py
@@ -134,6 +134,7 @@ for paper in tqdm(feed['papers']):
         log(f'Using data from previous issue: {json.dumps(prev_data, ensure_ascii=False)}')
         paper["data"] = prev_data
     else:
+        log('Querying the API.')
         abs = paper["abstract"][:3000]
         prompt = f"Read abstract of the ML paper and return a JSON with fields: 'desc': explanation of the paper in Russian (4 sentences), use correct machine learning terms, do not use terms like БЯМ. 'tags': array of tags related to article, 3 tags, but specific, not general like #ml or #ai. 'emoji': emoji that will reflect the theme of an article somehow, only one emoji. 'title': a slogan of a main idea of the article in Russian. Return only JSON and nothing else.\n\n{abs}"
         paper["data"] = get_data(prompt)
