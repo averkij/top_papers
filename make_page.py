294af39087c27618ecfc1bbd4ad68fc5929b31db
Sergei Averkiev <averoo@gmail.com>
2024-10-15 13:14:44 +0300

    Add google analytics to Chinese page.

--- make_page.py
+++ make_page.py
@@ -962,6 +962,13 @@ def make_html_zh(data):
     <!DOCTYPE html>
     <html lang="en">
     <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-C1CRWDNJ1J"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){{dataLayer.push(arguments);}}
+            gtag('js', new Date());
+            gtag('config', 'G-C1CRWDNJ1J');
+        </script>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Chinese reading task about ML</title>
