392f81b74717ea72f57ce49861ca538f79ecd49c
Sergei Averkiev <averoo@gmail.com>
2024-10-23 17:04:06 +0300

    Update temp page script.

--- temp.py
+++ temp.py
@@ -98,7 +98,14 @@ def get_week_info(date):
     return weekday, feed_date, prev_feed_date, next_feed_date
 
 weekday, feed_date, prev_feed_date, next_feed_date = get_week_info(helper.CURRENT_DATE)
+
 formatted_date = format_date(feed_date, format="d MMMM", locale="ru_RU")
+formatted_date_en = format_date(feed_date, format="d MMMM", locale="en_US")
+formatted_date_prev = format_date(prev_feed_date, format="d MMMM", locale="ru_RU")
+formatted_date_next = format_date(next_feed_date, format="d MMMM", locale="ru_RU")
+short_date_prev = prev_feed_date.strftime('%d.%m')
+short_date_next = next_feed_date.strftime('%d.%m')
+
 formatted_time_utc = helper.CURRENT_DATE.strftime("%Y-%m-%d %H:%M")
 
 link_prev = f"{prev_feed_date.strftime('%Y-%m-%d')}.html"
@@ -106,6 +113,7 @@ link_next = f"{next_feed_date.strftime('%Y-%m-%d')}.html"
 
 feed = {
     "date": formatted_date,
+    "date_en": formatted_date_en,
     "time_utc": formatted_time_utc,
     "weekday": weekday,
     "issue_id": _issue_id + 1,
@@ -113,6 +121,10 @@ feed = {
     "papers": papers,
     "link_prev": link_prev,
     "link_next": link_next,
+    "date_prev": formatted_date_prev,
+    "date_next": formatted_date_next,
+    "short_date_prev": short_date_prev,
+    "short_date_next": short_date_next,
 }
 
 for i, paper in enumerate(feed["papers"]):
@@ -723,8 +735,7 @@ def make_html(data):
             }
             
             .nav-item {
-                padding: 0.3em 0.8em;
-                font-size: 0.9em;
+                padding: 3px 3px;
             }
         }
         """
@@ -853,10 +864,10 @@ def make_html(data):
     </header>
     <div class="nav-menu">
         <div class="nav-container">
-            <span class="nav-item" id="nav-prev"><a href="d/{feed['link_prev']}">Назад</a></span>
-            <span class="nav-item" id="nav-next"><a href="d/{feed['link_next']}">Вперед</a></span>
-            <span class="nav-item" id="nav-weekly">Топ за неделю</span>
-            <span class="nav-item" id="nav-weekly">Топ за месяц</span>
+            <span class="nav-item" id="nav-prev"><a href="d/{feed['link_prev']}">⬅️ {feed['short_date_prev']}</a></span>
+            <span class="nav-item" id="nav-next"><a href="d/{feed['link_next']}">➡️ {feed['short_date_next']}</a></span>
+            <!--<span class="nav-item" id="nav-weekly">Топ за неделю</span>
+            <span class="nav-item" id="nav-weekly">Топ за месяц</span>-->
         </div>
     </div>
     <div class="container">
