2ffbf843434ee52fe8feaa162ad7ac5591ee2b51
Sergei Averkiev <averoo@gmail.com>
2024-11-05 11:20:40 +0300

    Update json script.

--- scripts/update_json.py
+++ scripts/update_json.py
@@ -266,3 +266,127 @@ for doc in prev_papers:
         indent=4,
     )
 # %%
+#update ratings
+import json
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from babel.dates import format_date
+from datetime import datetime, timezone, timedelta
+
+
+from glob import glob
+import json
+import os
+from datetime import datetime, timezone, timedelta
+
+import requests
+from babel.dates import format_date
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+import constants as con
+import helper
+
+#load all prev papers
+prev_papers = glob("./d/*.json")
+all_prev_papers = []
+for doc in prev_papers:
+    with open(doc, "r", encoding="utf-8") as fin:
+        prev_feed = json.load(fin)
+        for paper in prev_feed["papers"]:
+            # fix categories
+            if "categories" in paper["data"]:
+                paper["data"]["categories"] = [
+                    x for x in paper["data"]["categories"] if x not in con.EXCLUDE_CATS
+                ]
+                paper["data"]["categories"] = [
+                    x if x not in con.RENAME_CATS else con.RENAME_CATS[x]
+                    for x in paper["data"]["categories"]
+                ]
+                paper["data"]["categories"] = [
+                    f"#{x.replace('#','')}".lower() for x in paper["data"]["categories"]
+                ]
+            all_prev_papers.append(paper)
+
+print('all_prev_papers:', len(all_prev_papers))
+
+#actualize scores and dates
+for doc in prev_papers[1:]:
+    with open(doc, "r", encoding="utf-8") as fin:
+        feed = json.load(fin)
+        feed_date_str = f"{doc[4:14]}"
+        print(feed_date_str)
+
+        # feed_date = datetime.strptime(feed_date_str, "%Y-%m-%d %H:%M")
+
+        BASE_URL = f"https://huggingface.co/papers?date={feed_date_str}"
+
+        page = requests.get(BASE_URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        articles = soup.find_all("article")
+
+        hf_papers = []
+        articles_datas = []
+        for article in tqdm(articles):
+            h3 = article.find("h3")
+            a = h3.find("a")
+            title = a.text
+            link = a["href"]
+            url = f"https://huggingface.co{link}"
+            page_data = helper.extract_page_data(url)
+            abstract = page_data["abstract"]
+            pub_date = page_data["pub_date"]
+
+            published_date = datetime.strptime(pub_date, "%Y-%m-%d")
+            hf_papers.append(
+                {
+                    "id": url,
+                    "title": title,
+                    "url": url,
+                    "abstract": abstract,
+                    "score": helper.try_get_score(article),
+                    "pub_date": pub_date,
+                    "pub_date_card": {
+                        "ru": format_date(published_date, format="d MMMM", locale="ru_RU"),
+                        "en": format_date(published_date, format="MMMM d", locale="en_US"),
+                        "zh": helper.format_date_zh(published_date),
+                    },
+                    "hash": helper.get_hash(url),
+                }
+            )
+
+        updated_papers = []
+        missed_papers_count = len(hf_papers)
+        for hf_paper in hf_papers:
+            for prev_paper in all_prev_papers:
+                if hf_paper["url"] == prev_paper["url"]:
+
+                    to_update = prev_paper
+
+                    to_update["score"] = hf_paper["score"]
+                    to_update["pub_date"] = hf_paper["pub_date"]
+                    published_date = datetime.strptime(hf_paper["pub_date"], "%Y-%m-%d")
+                    to_update["pub_date_card"] = {'ru': format_date(published_date, format="d MMMM", locale="ru_RU"),
+                              'en': format_date(published_date, format="MMMM d", locale="en_US"),
+                              'zh': helper.format_date_zh(published_date)}
+                    
+                    updated_papers.append(to_update)
+
+                    missed_papers_count -= 1
+                    break
+        
+        feed["papers"] = updated_papers
+
+    print('Missed papers:', missed_papers_count)   
+    print('Saving the feed...')
+    json.dump(
+        feed,
+        open(doc, "w", encoding="utf-8"),
+        ensure_ascii=False,
+        indent=4,
+    )
+
+    # break
+#%%
+updated_papers
